e7755b7e8567e90c1e99db7926e2d14c5176d2ec
Roger Oba <rogerluan.oba@gmail.com>
2020-11-02 19:31:36 -0300

    Add support to JWT.

--- src/fetch_app_status.rb
+++ src/fetch_app_status.rb
@@ -1,14 +1,64 @@
 # frozen_string_literal: true
 
+# Needed to load gems from Gemfile
+require 'rubygems'
+require 'bundler/setup'
+
 require "spaceship"
 require "json"
 
+# Constants
+def bundle_id
+  ENV["BUNDLE_IDENTIFIER"]
+end
+
+def itc_username
+  ENV["ITC_USERNAME"]
+end
+
+def itc_password
+  ENV["ITC_PASSWORD"]
+end
+
+def spaceship_connect_api_key_id
+  ENV["SPACESHIP_CONNECT_API_KEY_ID"]
+end
+
+def spaceship_connect_api_issuer_id
+  ENV["SPACESHIP_CONNECT_API_ISSUER_ID"]
+end
+
+def spaceship_connect_api_key
+  ENV["SPACESHIP_CONNECT_API_KEY"]
+end
+
+def uses_app_store_connect_auth_token
+  !spaceship_connect_api_key_id.nil? && !spaceship_connect_api_issuer_id.nil? && !spaceship_connect_api_key.nil?
+end
+
+def uses_app_store_connect_auth_credentials
+  !uses_app_store_connect_auth_token && !itc_username.nil?
+end
+
+def itc_team_id_array
+  # Split team_id
+  ENV["ITC_TEAM_IDS"].to_s.split(",")
+end
+
+unless uses_app_store_connect_auth_token || uses_app_store_connect_auth_credentials
+  puts "Couldn't find valid authentication token or credentials."
+  exit
+end
+
 def get_version_info(app)
   latest_version_info = app.get_latest_app_store_version(platform: Spaceship::ConnectAPI::Platform::IOS)
-  icon_url = latest_version_info.store_icon["templateUrl"]
-  icon_url["{w}"] = "340"
-  icon_url["{h}"] = "340"
-  icon_url["{f}"] = "png"
+  # FIXME: Analyze how to read the store icon. See https://github.com/fastlane/fastlane/issues/17370 for more info.
+  if uses_app_store_connect_auth_credentials
+    icon_url = latest_version_info.store_icon["templateUrl"]
+    icon_url["{w}"] = "340"
+    icon_url["{h}"] = "340"
+    icon_url["{f}"] = "png"
+  end
   {
     "name" => app.name,
     "version" => latest_version_info.version_string,
@@ -28,19 +78,15 @@ def get_app_version_from(bundle_id)
   apps.map { |app| get_version_info(app) }
 end
 
-# Constants
-itc_username = ENV["ITC_USERNAME"]
-itc_password = ENV["ITC_PASSWORD"]
-# Split team_id
-itc_team_id_array = ENV["ITC_TEAM_IDS"].to_s.split(",")
-bundle_id = ENV["BUNDLE_IDENTIFIER"]
-
-unless itc_username
-  puts "did not find username"
-  exit
+if uses_app_store_connect_auth_token
+  # FIXME: This is a temporary setup. See https://github.com/fastlane/fastlane/pull/17369 for more info.
+  Spaceship::ConnectAPI.token = Spaceship::ConnectAPI::Token.create(key_id: spaceship_connect_api_key_id, issuer_id: spaceship_connect_api_issuer_id, key: spaceship_connect_api_key)
+  # Should be replaced with this once the PR above is merged:
+  # Spaceship::ConnectAPI.auth(key_id: spaceship_connect_api_key_id, issuer_id: spaceship_connect_api_issuer_id, key: spaceship_connect_api_key)
+else
+  Spaceship::ConnectAPI.login(itc_username, itc_password)
 end
 
-Spaceship::ConnectAPI.login(itc_username, itc_password)
 # All json data
 versions = []
 
